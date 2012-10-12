06e2a6a64970b5b89c9636785555a1b7b06e456d
Magnus Grimstvedt Saltnes <magnussaltnes@gmail.com>
2012-10-12 11:28:53 +0200

    Working on AbstractPlayer.

--- src/octo_ninja/player/AbstractPlayer.java
+++ src/octo_ninja/player/AbstractPlayer.java
@@ -1,21 +1,37 @@
 package octo_ninja.player;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Set;
 
 import octo_ninja.model.Board;
+import octo_ninja.model.GameState;
 import octo_ninja.model.IPlayer;
+import octo_ninja.model.Move;
 import octo_ninja.model.Piece;
 
 /**An infrastructure for using a Player as a process which communicates its moves over STDIN/STDOUT. */
 public abstract class AbstractPlayer implements IPlayer {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		
 		Board board = new Board();
 		Set<Piece> pieces = Piece.getPieceSet();
+		GameState state = new GameState(board, null, null, pieces);
+		AbstractPlayer player = getInstance();
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		
+		Move move = player.chooseMove(state);
+		Piece piece = move.getChosenPiece();
+		System.out.println(piece.toString());
+		
 		while(!board.isWon()){
-			
+			String response = reader.readLine();
 		}
 	}
+
+	protected static abstract AbstractPlayer getInstance();
 
 
 }
