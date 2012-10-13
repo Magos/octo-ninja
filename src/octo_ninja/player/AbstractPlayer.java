30c5f7ca091bc71eb4916863371cbb42cafcc970
Magnus Grimstvedt Saltnes <magnussaltnes@gmail.com>
2012-10-13 09:39:15 +0200

    Trying to mesh with Gamemaster.

--- src/octo_ninja/player/AbstractPlayer.java
+++ src/octo_ninja/player/AbstractPlayer.java
@@ -23,17 +23,26 @@ public abstract class AbstractPlayer implements IPlayer {
 		Set<Piece> pieces = Piece.getPieceSet();
 		GameState state = new GameState(board, null, null, pieces);
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
+		
+		String first = reader.readLine();
+		if(first.equals("Make first move.")){
 		Move move = chooseMove(state);
 		Piece piece = move.getChosenPiece();
 		pieces.remove(piece);
 		state = new GameState(board,piece,null,pieces);
 		System.out.println(piece.toString());
-
+		}else{
+			Piece oppnentsChoice = Piece.PieceFromString(first);
+			if(oppnentsChoice == null){
+				throw new RuntimeException("Opponent chose invalid piece.");
+			}
+			pieces.remove(oppnentsChoice);
+			state = new GameState(board, oppnentsChoice,this,pieces);
+		}
 
 
 		while(!board.isWon()){
-			move = getOpponentMove(reader);
+			Move move = getOpponentMove(reader);
 			state = applyMove(move, state);
 			move = chooseMove(state);
 			System.out.println(move.getX() + " " + move.getY());
