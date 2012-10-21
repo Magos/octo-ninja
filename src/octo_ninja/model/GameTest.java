8fa7938a45db2583dde6c40135384939d357d2c4
Magnus Grimstvedt Saltnes <magnussaltnes@gmail.com>
2012-10-21 15:09:38 +0200

    Added timeout to GameTest.

--- src/octo_ninja/model/GameTest.java
+++ src/octo_ninja/model/GameTest.java
@@ -1,14 +1,13 @@
 package octo_ninja.model;
 
-import static org.junit.Assert.*;
-
 import octo_ninja.player.RandomPlayer;
 
 import org.junit.Test;
 
 public class GameTest {
 
-	@Test
+	//Basic as can be: Run a game by random players and verify nothing throws exceptions and it ends in a somewhat reasonable timeframe.
+	@Test(timeout=500)
 	public void testRun() {
 		Game game = new Game(new RandomPlayer(), new RandomPlayer());
 		game.run();
