cbfd959cc9580500cec33e3f3d39815c4dc9a62e
Magnus Grimstvedt Saltnes <magnussaltnes@gmail.com>
2012-10-21 17:44:02 +0200

    Starting on a command line interface for making any kind of game.

--- src/octo_ninja/model/Game.java
+++ src/octo_ninja/model/Game.java
@@ -1,5 +1,7 @@
 package octo_ninja.model;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 import octo_ninja.player.RandomPlayer;
@@ -28,7 +30,7 @@ public class Game implements Runnable {
 		Board board = new Board();
 		GameState state = new GameState(board, null, remainingPieces);
 		int turn = 0;
-		while(turn < 17){
+		while(turn < 17 && !state.isWon() && state.getPieces().size() > 0){
 			Move move =players[turn % 2].chooseMove(state);
 			state = state.applyMove(move);
 			if(state.isWon()){
@@ -45,8 +47,52 @@ public class Game implements Runnable {
 	}
 	
 	public static void main(String[] args) {
-		Game game = new Game(new RandomPlayer(),new HumanPlayer());
-		new Thread(game).start();
+		Player player1 = null;
+		Class<Player> ply1Class = null;
+		try {
+			 ply1Class = (Class<Player>) Class.forName(args[0]);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		try {
+			player1 = ply1Class.newInstance();
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		}
+		
+		Player player2 = null;
+		Class<Player> ply2Class = null;
+		
+		try {
+			 ply2Class = (Class<Player>) Class.forName(args[1]);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		try {
+			player2 = ply2Class.newInstance();
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		}
+		int n = 100;
+		
+		if(args[3] != null && args[3].equalsIgnoreCase("-n")){
+			n = Integer.parseInt(args[4]);
+		}
+		Map<Result,Integer> results = new HashMap<Result,Integer>();
+		for (Result res : Result.values()) {
+			results.put(res, 0);
+		}
+		for (int i = 0; i < n; i++) {
+			Game game = new Game(player1, player2);
+			game.run();
+			Result res = game.getResult();
+			results.put(res, results.get(res)+1);
+		}
+		System.out.println(results);
 	}
 
 }
